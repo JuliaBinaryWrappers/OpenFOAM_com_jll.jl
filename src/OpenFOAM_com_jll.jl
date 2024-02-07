f62d6aeca99c3f11ac799e0ba2087726c7398616
jlbuild <juliabuildbot@gmail.com>
2024-02-07 00:11:40 +0000

    OpenFOAM_com_jll build 2312.0.0+0

---
+++ src/OpenFOAM_com_jll.jl
@@ -0,0 +1,10 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule OpenFOAM_com_jll
+using Base
+using Base: UUID
+using MPIPreferences
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("OpenFOAM_com")
+JLLWrappers.@generate_main_file("OpenFOAM_com", UUID("fd4958d8-5e5c-566c-bfe6-815b1420e10d"))
+end  # module OpenFOAM_com_jll
